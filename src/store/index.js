526aec72e39dc42a03ab1422f141011dd978a909
Aman Gupta <amanguptaprogrammer@gmail.com>
2022-08-25 18:00:28 +0530

    Counter buttons in effect using Redux dispatch

--- src/store/index.js
+++ src/store/index.js
@@ -4,11 +4,11 @@ const counterReducer = (state = { counter: 0 }, action) => {
   switch (action.type) {
     case "INCREMENT":
       return {
-        count: state.counter + 1,
+        counter: state.counter + 1,
       };
     case "DECREMENT":
       return {
-        count: state.counter - 1,
+        counter: state.counter - 1,
       };
     default:
       return state;
